a3a82e59daf4bfcbfbbbac5fa06ecb505da75dbc
KaelDucatti <ducatti860@gmail.com>
2026-01-07 17:52:08 -0300

    feat(campaign): add Campaing construct

--- internal/domain/campaign/campaign.go
+++ internal/domain/campaign/campaign.go
@@ -7,9 +7,24 @@ type Contact struct {
 }
 
 type Campaign struct {
-	Id 			string
+	Id 			int
 	Name 		string
 	Content		string
 	CreatedAt 	time.Time
 	Contacts 	[]Contact
+}
+
+func NewCampaign(name, content string, emails []string) *Campaign {
+	contacts := make([]Contact, len(emails))
+	for index, value := range emails {
+		contacts[index].Email = value
+	}	
+
+	return &Campaign{
+		Id: 1,
+		Name: name,
+		Content: content,
+		CreatedAt: time.Now(),
+		Contacts: contacts,
+	}
 }
