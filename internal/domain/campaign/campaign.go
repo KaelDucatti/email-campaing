ca536015e56c4596b0f84025af6bc75fcf8e55e2
KaelDucatti <ducatti860@gmail.com>
2026-01-11 19:12:38 -0300

    refactor: improve campaign tests e error handling

--- internal/domain/campaign/campaign.go
+++ internal/domain/campaign/campaign.go
@@ -1,30 +1,43 @@
 package campaign
 
-import "time"
+import (
+	"errors"
+	"time"
+
+	"github.com/rs/xid"
+)
 
 type Contact struct {
 	Email string
 }
 
 type Campaign struct {
-	Id 			int
+	Id 			string
 	Name 		string
 	Content		string
 	CreatedAt 	time.Time
 	Contacts 	[]Contact
 }
 
-func NewCampaign(name, content string, emails []string) *Campaign {
+func NewCampaign(name, content string, emails []string) (*Campaign, error) {
+	if name == "" {
+		return nil, errors.New("Name is required.")
+	} else if content == "" {
+		return nil, errors.New("Content is required.")
+	} else if len(emails) == 0 {
+		return nil, errors.New("E-mail is required.")
+	}
+	
 	contacts := make([]Contact, len(emails))
 	for index, value := range emails {
 		contacts[index].Email = value
 	}	
 
 	return &Campaign{
-		Id: 1,
+		Id: xid.New().String(),
 		Name: name,
-		Content: content,
+		Content: content,	
 		CreatedAt: time.Now(),
 		Contacts: contacts,
-	}
+	}, nil
 }
--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -2,21 +2,61 @@ package campaign
 
 import (
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestNewCampaing(t *testing.T) {
-	name := "Campaign X"
-	content := "Body"
-	contacts := []string{"example1@test.com", "example2@test.com"}
-	campaign := NewCampaign(name, content, contacts)
+var (
+	name 		= "Campaign X"
+	content 	= "Body"
+	contacts 	= []string{"example1@test.com", "example2@test.com"}
+)
 
-	if campaign.Id != 1 {
-		t.Errorf("Expected: 1")
-	} else if campaign.Name != name {
-		t.Errorf("Expected: correct name\n")
-	} else if campaign.Content != content {
-		t.Errorf("Expected correct content\n")
-	} else if len(campaign.Contacts) != len(contacts) {
-		t.Errorf("Expected correct content\n")
-	}
+func TestNewCampaign(t *testing.T) {
+	t.Run("should create new campaign", func(t *testing.T){
+		assert := assert.New(t)
+		require := require.New(t)
+		now := time.Now().Add(-time.Minute)
+		
+		campaign, err := NewCampaign(name, content, contacts)
+
+		require.NoError(err)
+		assert.NotNil(campaign.Id)
+		assert.Greater(campaign.CreatedAt, now)
+		assert.Equal(name, campaign.Name)
+		assert.Equal(content, campaign.Content)
+		assert.Equal(len(contacts), len(campaign.Contacts))
+	})
+
+	t.Run("validation errors", func(t *testing.T){
+		t.Run("should return error when name is empty", func(t *testing.T){
+			assert := assert.New(t)
+			require := require.New(t)
+
+			_, err := NewCampaign("", content, contacts)
+
+			require.Error(err)
+			assert.EqualError(err, "Name is required.")
+		})
+		t.Run("should return error when content is empty", func(t *testing.T){
+			assert := assert.New(t)
+			require := require.New(t)
+
+			_, err := NewCampaign(name, "", contacts)
+
+			require.Error(err)
+			assert.EqualError(err, "Content is required.")
+		})
+		t.Run("should return error when contacts is 0", func(t *testing.T){
+			assert := assert.New(t)
+			require := require.New(t)
+
+			_, err := NewCampaign(name, content, []string{})
+
+			require.Error(err)
+			assert.EqualError(err, "E-mail is required.")
+		})
+	})
 }
